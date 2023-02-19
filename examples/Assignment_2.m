417c0abc1d52cf097ece8c64ec6b4d73aa338a07
Kevin-Xue01 <59587075+Kevin-Xue01@users.noreply.github.com>
2023-02-19 10:03:47 -0800

    Delete examples directory

--- examples/Assignment_2.m
+++
@@ -1,7 +0,0 @@
-t=0:0.1:10; % forming a time access from 0 to 10 with 0.1 time steps
-x=exp(-t).*cos((pi/2).*t);
-
-plot(t,x);
-title('Real part of signal, x(t) = exp(-(1 + (j*π)/2)t)')
-xlabel('t')
-ylabel('Re[x(t)]')
